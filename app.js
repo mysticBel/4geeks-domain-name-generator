665497a8c3bed6685d04ac7ea354a0e59cf13454
mysticBel <dnxbel@gmail.com>
2024-08-02 05:04:44 -0500

    subiendo proyecto

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Any string");
+//console.log("Any string");
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+      }
+    }
+  }
+}
